c511eb9b21531054e0310942d22291a3dc364076
Sushant Khandelwal <sushankh@amazon.com>
2023-10-10 13:59:17 +0530

    added port 8080 sg to isntance

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "my-ec2-vm" {
   count = 1
   key_name      = "terraform-key"
   user_data = file("apache-install.sh")  
-  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id, aws_security_group.vpc-8080]
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id, aws_security_group.vpc-8080.id]
   tags = {
     "Name" = "Terraform-Cloud-${count.index}"
   }
