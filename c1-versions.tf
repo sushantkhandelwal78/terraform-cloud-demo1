9c4f048215e4e6ab04fa717de4b54bf8788c45e4
Sushant Khandelwal <sushankh@amazon.com>
2023-10-10 13:46:52 +0530

    new commit

--- c1-versions.tf
+++ c1-versions.tf
@@ -1,10 +1,10 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.4" 
+  required_version = ">= 1.6" 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
